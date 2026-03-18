2db966de4ae9ee5f4b57db070eb2710691c87eb7
Jittaphon <artjitt2512@gmail.com>
2026-03-18 19:25:19 +0700

    change path

--- frontend/src/api/dcdepartmentAPI.js
+++ frontend/src/api/dcdepartmentAPI.js
@@ -2,7 +2,10 @@ import axiosClient from "./axiosClient";
 const dcdepartmentAPI = {
 postSaveScreeningResults(data) {
   return axiosClient.post(`api/v1/kpi/save-screening-results`, data);
-},  
+}, 
+postSaveScreeningResults(data) {
+  return axiosClient.post(`api/v1/kpi/save-screening-results`, data);
+},   
 getScreeningResults(params) {
   return axiosClient.get(`api/v1/kpi/get-screening-results`, { params });
 
